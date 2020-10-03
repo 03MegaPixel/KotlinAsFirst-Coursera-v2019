37c9a1c18cb3ca58cfabaa36b70fff1aece9e610
03MegaPixel <antongapeev@gmail.com>
2020-10-04 00:39:06 +0300

    Решены все задачи Второго урока (В этот раз полностью)) )

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -3,6 +3,7 @@
 package lesson2.task2
 
 import lesson1.task1.sqr
+import kotlin.math.*
 
 /**
  * Пример
@@ -18,7 +19,13 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = TODO()
+fun isNumberHappy(number: Int): Boolean {
+    var n1 = number / 1000
+    var n2 = number / 100 % 10
+    var n3 = number / 10 % 10
+    var n4 = number % 10
+    return (n1 + n2) == (n3 + n4)
+}
 
 /**
  * Простая
@@ -27,7 +34,8 @@ fun isNumberHappy(number: Int): Boolean = TODO()
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  * Считать, что ферзи не могут загораживать друг друга.
  */
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean =
+    x1 == x2 || y1 == y2 || x1 - y1 == x2 - y2 || x1 + y1 == x2 + y2
 
 
 /**
@@ -35,8 +43,24 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
  *
  * Дан номер месяца (от 1 до 12 включительно) и год (положительный).
  * Вернуть число дней в этом месяце этого года по григорианскому календарю.
+ *      год, номер которого кратен 400, — високосный;
+ *      остальные годы, номер которых кратен 100, — невисокосные (например, годы 1700, 1800, 1900, 2100);
+ *      остальные годы, номер которых кратен 4, — високосные.
  */
-fun daysInMonth(month: Int, year: Int): Int = TODO()
+fun daysInMonth(month: Int, year: Int): Int {
+    var lYear: Boolean = when {
+        year % 400.0 == 0.0 -> true
+        year % 100.0 == 0.0 -> false
+        year % 4.0 == 0.0 -> true
+        else -> false
+    }
+    return when {
+        month == 2 -> if (lYear) 29 else 28
+        month % 2.0 == 0.0 && month in 1..7 -> 30
+        month % 2.0 != 0.0 && month in 8..12 -> 30
+        else -> 31
+    }
+}
 
 /**
  * Средняя
@@ -48,7 +72,10 @@ fun daysInMonth(month: Int, year: Int): Int = TODO()
 fun circleInside(
     x1: Double, y1: Double, r1: Double,
     x2: Double, y2: Double, r2: Double
-): Boolean = TODO()
+): Boolean {
+    var smeshenie = sqrt((x2 - x1).pow(2) + (y2 - y1).pow(2))
+    return r1 + smeshenie <= r2
+}
 
 /**
  * Средняя
@@ -56,7 +83,12 @@ fun circleInside(
  * Определить, пройдет ли кирпич со сторонами а, b, c сквозь прямоугольное отверстие в стене со сторонами r и s.
  * Стороны отверстия должны быть параллельны граням кирпича.
  * Считать, что совпадения длин сторон достаточно для прохождения кирпича, т.е., например,
- * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
+ * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.      120-5-10   10-4
  * Вернуть true, если кирпич пройдёт
  */
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = TODO()
+fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean {
+    var min1 = min(a, min(b, c))
+    var min2 = a + b + c - min1 - max(a, max(b, c))
+    return (min2 <= max(r, s)) && (min1 <= min(r, s))
+
+}
